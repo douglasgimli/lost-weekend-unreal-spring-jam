5bbb1c5ca41bbe5649ea6ed34cfa0579618a557b
DouglasGimli <douglasgimli@gmail.com>
2017-04-02 19:57:37 -0300

    Final build with correct name

--- Build/Android/src/com/lostweekend/baddayraccoon/OBBData.java
+++ Build/Android/src/com/lostweekend/baddayraccoon/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-64096482L // the length of the file in bytes
+64096513L // the length of the file in bytes
 )
 };
 };
